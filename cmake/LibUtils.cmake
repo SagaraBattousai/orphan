151777bd5e65174f24b5fec77e647355a78bdd7b
James Calo <jamesafcalo@gmail.com>
2023-01-10 11:31:55 +0000

    Initial project directory setup for cmake C/C++

---
+++ cmake/LibUtils.cmake
@@ -0,0 +1,13 @@
+#TODO: Can I make it a symlink??
+
+#Keeping others just for now, will be removed in the future. Will use copy as 
+#copy if different was causing an issue ( I think/remember)
+function(auto_copy_dlls_for_target target)
+  add_custom_command(TARGET ${target} 
+    POST_BUILD 
+    COMMAND ${CMAKE_COMMAND} -E copy
+    $<TARGET_RUNTIME_DLLS:${target}>
+    $<TARGET_FILE_DIR:${target}>
+  COMMAND_EXPAND_LISTS
+)
+endfunction()
