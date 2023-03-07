7192c4944a98361be4c879f08d12de8fe7b87551
James Calo <jamesafcalo@gmail.com>
2023-03-07 00:39:37 +0000

    Changed Sphinx and Doxygen target names to include ORPHAN to avoid targets with the same name

--- app/main.c
+++ app/main.c
@@ -1,21 +1,16 @@
 
+/*
 #include <iostream>
 #include <vector>
 #include <cstddef>
 #include <cstdint>
-
-#include <orphan/orphan-bigint.h>
-#include <orphan/orphan-integral.h>
+*/
+//Not currently C safe!
+//#include <orphan/orphan-bigint.h>
+//#include <orphan/orphan-integral.h>
 
 
 int main()
 {
-	orphan::BigInt x{ (unsigned long long) 32 };
-	orphan::BigInt y{ (unsigned long long) 154 };
-
-	char z = 4 << (unsigned)3;
-
-	std::cout << (x+y) << " = " << 32 + 154 << std::endl;
-
 	return 0;
 }
